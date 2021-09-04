f44f14009e562f30564316070671da9a75f5f254
Quadrant <Quadrant@DESKTOP-5K9DCI5>
2021-09-04 20:38:23 +0530

    Add project files.

---
+++ GridDemo/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using GridDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GridDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Details()
+        {
+            SchemeEntities GVDB = new SchemeEntities();
+
+            var SchemeList = (from scheme in GVDB.SchemeMasters
+                              select scheme).ToList();
+            return View(SchemeList);
+        }
+    }
+}
